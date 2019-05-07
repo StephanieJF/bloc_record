2b38a03dcf0d42695c41949abe571481218d7b8b
Stephanie Fischler <sjfischler@gmail.com>
2019-05-07 19:26:26 -0400

    method_missing WIP2

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -97,13 +97,13 @@ module Selection
     rows_to_array(rows)
   end
 
-	def method_missing(m, *args, &block)
-		if m.to_s =~ /^find_by_(.*)$/
-			find_by_($1.to_sym => *args.first)
-		else
-			super
-		end
-	end
+	# def method_missing(m, *args, &block)
+	# 	if m.to_s =~ /^find_by_(.*)$/
+	# 		find_by_($1.to_sym => args.first)
+	# 	else
+	# 		super
+	# 	end
+	# end
 
 	private
 
