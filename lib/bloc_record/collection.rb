f49f7cbe42f327e872bcff7a86aa4ff659df2959
Stephanie Fischler <sjfischler@gmail.com>
2019-05-22 18:06:31 -0400

    Complete Update Assignment

--- lib/bloc_record/collection.rb
+++ lib/bloc_record/collection.rb
@@ -4,5 +4,19 @@ module BlocRecord
 			ids = self.map(&:id)
 			self.any? ? self.first.class.update(ids, updates) : false
 		end
+
+		def take
+			self.class.take_one
+		end
+
+		def where(args)
+			ids = self.map(&:id)
+			self.first.class.where(args)
+		end
+
+		def not(args)
+			ids = self.map(&:id)
+			self.first.class.where.not(args)
+		end
 	end
 end
